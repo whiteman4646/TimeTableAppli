b4b1f6513b046dccf7c1c388b2ca7850fe4704b6
hayato maeda <h.maeda1711@morijyobi.ac.jp>
2018-11-06 14:58:08 +0900

    test featureMaeda commit

--- src/DepartmentCourse.java
+++ src/DepartmentCourse.java
@@ -16,10 +16,11 @@ import javafx.stage.Stage;
  */
 public class DepartmentCourse extends Application
 {
+
     public static void main(String[] args) {
+
         launch(args);
     }
-
 
     @Override
     public void start(Stage primaryStage) throws Exception
