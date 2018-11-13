4beb3e37368dd40f8c5e2c2e201e17c60061a3a6
Kiyoshi Kumagai <baervalley.key@gmail.com>
2018-11-13 10:04:23 +0900

    教員・教科の登録機能完成

--- src/Fxml/STController.java
+++ src/Fxml/STController.java
@@ -1,5 +1,8 @@
 package Fxml;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import dao.SubjectTeacherDAO;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
@@ -12,11 +15,38 @@ public class STController {
 	private Button subTeaEntButton;
 
 	@FXML
-	private TextField teatext;
+	private TextField teatext1, teatext2, teatext3 ,teatext4, teatext5,
+					   subtext1, subtext2, subtext3, subtext4, subtext5;
+
+	List<TextField> teaList = new ArrayList<TextField>(5);
+	List<TextField> subList = new ArrayList<TextField>(5);
 
 	@FXML
     public void subTeaEntry(ActionEvent e){
-		System.out.println("登録が押されました。");
-		SubjectTeacherDAO.insertSubject(teatext.getText());
+		teaList.add(teatext1);
+		teaList.add(teatext2);
+		teaList.add(teatext3);
+		teaList.add(teatext4);
+		teaList.add(teatext5);
+
+		subList.add(subtext1);
+		subList.add(subtext2);
+		subList.add(subtext3);
+		subList.add(subtext4);
+		subList.add(subtext5);
+
+		//System.out.println("登録が押されました。");
+		for(TextField t: teaList) {
+			if(t.getText().equals("")) {
+				continue;
+			}
+			SubjectTeacherDAO.insertTeacher(t.getText());
+		}
+		for(TextField s: subList) {
+			if(s.getText().equals("")) {
+				continue;
+			}
+			SubjectTeacherDAO.insertSubject(s.getText());
+		}
     }
 }
