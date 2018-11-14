af3cdec692037886b6dbf8888011fe66f0cbd21e
hayato maeda <h.maeda1711@morijyobi.ac.jp>
2018-11-14 13:39:07 +0900

    学科コース/教室データの一覧表示成功

---
+++ src/dto/DepartmentCourse.java
@@ -0,0 +1,28 @@
+package dto;
+
+public class DepartmentCourse {
+	private int dcid;
+	private String dcname;
+
+	public DepartmentCourse(int dcid, String dcname) {
+		super();
+		this.dcid = dcid;
+		this.dcname = dcname;
+	}
+
+	public int getDcid() {
+		return dcid;
+	}
+
+	public void setId(int dcid) {
+		this.dcid = dcid;
+	}
+
+	public String getDcname() {
+		return dcname;
+	}
+
+	public void setDcname(String dcname) {
+		this.dcname = dcname;
+	}
+}
