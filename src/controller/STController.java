e3e80fc4a031530a2d1baa8a2bd45a5633fc69ee
ryo maruo <r.ryou1711@morijyobi.ac.jp>
2018-12-10 14:27:50 +0900

    科目教員のメニューバーにファイルを追

--- src/controller/STController.java
+++ src/controller/STController.java
@@ -1,5 +1,6 @@
 package controller;
 
+import java.io.File;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
@@ -19,6 +20,8 @@ import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.stage.FileChooser;
+
 
 public class STController implements Initializable {
 	private final String cttPage = "CreateTime.fxml";
@@ -29,10 +32,11 @@ public class STController implements Initializable {
 	private final String helpPage = "help.fxml";
 
 	@FXML
-	private Menu cttmenu, registmenu, deleteMenu, helpMenu;
+	private Menu cttmenu, registmenu, deleteMenu, helpMenu, fileopen;
 	@FXML
 	private MenuItem cttmenuitem, dcregimenuItem, crregimenuItem,
-	dcdeleMenuItem, crdeleMenuItem, nexthelpMenuItem, helpMenuItem;
+	dcdeleMenuItem, crdeleMenuItem, nexthelpMenuItem, helpMenuItem,fileop;
+
 
 	@FXML
 	private Button subTeaEntButton;
@@ -130,5 +134,18 @@ public class STController implements Initializable {
 	@FXML
 	public void Nextctt() {
 		CreateTimetableMain.getInstance().setPage(cttPage);
+	}
+	@FXML
+	protected void nextfile(ActionEvent a) {
+		FileChooser fileChooser = new FileChooser();
+		fileChooser.setTitle("ファイルを開く");
+		fileChooser.setInitialDirectory(
+	            new File(System.getProperty("user.home"))
+	        );
+		File file = fileChooser.showOpenDialog(null);
+
+		String url = "file:///"+file.getPath();
+
+		System.out.println(url);
 	}
 }
