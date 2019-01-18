2dfa40a79ab77857db25f99831cc7c437fa43f9a
Kiyoshi Kumagai <baervalley.key@gmail.com>
2019-01-18 11:18:24 +0900

    印刷確認の教室が表示されない不具合を解消

--- src/controller/ConfirmationController.java
+++ src/controller/ConfirmationController.java
@@ -159,6 +159,7 @@ public class ConfirmationController implements Initializable{
 		result = FXCollections.observableArrayList(new ArrayList<Timetable>());
 		for(int i = 0; i < 7; i++) {
 			timetable = TimetableDAO.selectTimeatableChoiceCR(num, i + 1);
+			result = oList(timetable, i);
 		}
 		return result;
 	}
